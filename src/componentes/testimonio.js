5edc85215f3353dfdb717d5ee5dd8145c144a721
Juan Dluyz <juansedp2015@gmail.com>
2023-10-29 23:08:48 -0500

    Todos los testimonios + props

--- src/componentes/testimonio.js
+++ src/componentes/testimonio.js
@@ -1,18 +1,15 @@
 import React from "react";
 
-function Testimonio(){
+function Testimonio(props){
     return(
         <div className="contenedor-testimonio">
             <img className="imagen-testimonio"
-            src={require("../imagenes/testimonio-primero.png")}
+            src={require(`../imagenes/testimonio-${props.imagen}.png`)}
             alt="primera foto"/>
             <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio"> Xiangling from Liyue</p>
-                <p className="cargo-testimonio">Software Engineer at Spotify</p>
-                <p className="texto-testimonio">"I've always struggled with learning JavaScript. I've taken many courses but 
-                freeCodeCamp's course was the one which stuck. Studying JavaScript as well as data structures and algorithms 
-                on freeCodeCamp gave me the skills and confidence I needed to land my dream job as a software engineer 
-                at Spotify."</p>
+                <p className="nombre-testimonio"> {props.nombre} from {props.pais}</p>
+                <p className="cargo-testimonio">{props.cargo} at {props.empresa}</p>
+                <p className="texto-testimonio">"{props.testimonio}"</p>
 
 
             </div>
