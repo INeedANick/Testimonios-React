1ba9a2924f3773cad1c1b7262fe512cb1f5d7a0a
Juan Dluyz <juansedp2015@gmail.com>
2023-10-29 22:32:37 -0500

    contenedor de testimonios + imagen + texto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Testimonio from "./componentes/testimonio"
+import "../src/hojas-de-estilos/Testimonio.css"
 
 function App() {
   return (
     <div className="App">
-      <div className='conteneder-principal'>
+      <div className='contenedor-principal'>
         <h1>Here is what our alumni say about freeCodeCamp:</h1>
         <Testimonio />
       </div>
